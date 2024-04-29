da813b4de5c8c72eb8a74d15174a8d27a95998c3
Stelios Syrtagias <85074265+stelios996@users.noreply.github.com>
2024-04-29 23:25:44 +0300

    Update environment.development.ts

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     firebaseUrl: 'https://notes-manager-5c037-default-rtdb.europe-west1.firebasedatabase.app/notes.json',
     firebaseModUrl: 'https://notes-manager-5c037-default-rtdb.europe-west1.firebasedatabase.app/notes/',
-    firebaseSignUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDdCy7c5Sl3gxxK7BXhX9bWPpoV5Oun5EQ',
-    firebaseSignInUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDdCy7c5Sl3gxxK7BXhX9bWPpoV5Oun5EQ'
+    firebaseSignUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=API_KEY',
+    firebaseSignInUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=API_KEY'
 };
